54af31b23f0207d9ea431ca395dc1c63d839907e
Poxie <poxen12@hotmail.com>
2022-05-26 00:06:34 +0200

    Made navbar responsive

--- frontend/components/navbar/Navbar.tsx
+++ frontend/components/navbar/Navbar.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDeviceType } from '../../hooks/useDeviceType';
+import { HamIcon } from '../../icons/HamIcon';
 import styles from '../../styles/Navbar.module.scss';
 import { NavbarOptions } from './NavbarOptions';
 import { NavbarTabs } from './NavbarTabs';
 import { NavbarTitle } from './NavbarTitle';
 
 export const Navbar = () => {
+    const device = useDeviceType();
+    const [show, setShow] = useState(false);
+
+    // Methods for updating show state
+    const toggle = () => setShow(!show);
+    const close = () => setShow(false);
+
+    // Updating state if width of screen change
+    useEffect(() => {
+        if(device !== 'mobile') close();
+    }, [device]);
+
+    const containerStyles = [
+        styles['container'],
+        show ? styles['show-mobile'] : ''
+    ].join(' ');
     return(
-        <div className={styles['container']}> 
+        <div className={containerStyles}> 
             <div className={styles['left']}>
+                <HamIcon onClick={toggle} className={styles['ham']} />
                 <NavbarTitle />
                 <NavbarTabs />
             </div>
