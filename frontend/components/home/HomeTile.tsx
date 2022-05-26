e6d4fa4c11f1a3202d7d3e3e9f3cf6478a0b5039
Poxie <poxen12@hotmail.com>
2022-05-27 00:30:13 +0200

    Added styles for home tiles

---
+++ frontend/components/home/HomeTile.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from '../../styles/Home.module.scss';
+
+export const HomeTile: React.FC<{
+    title: string;
+    description: string;
+    media: any;
+}> = ({ title, description, media }) => {
+    const [isHidden, setIsHidden] = useState(true);
+    const ref = useRef<HTMLDivElement>(null);
+
+    // Checking if content is within threshold within viewport
+    useEffect(() => {
+        const scroll = () => {
+            if(!ref.current) return;
+            const fromTop = ref.current.getBoundingClientRect().top;
+            const percent = fromTop / window.innerHeight;
+            
+            if(percent < .8) {
+                setIsHidden(false);
+            }
+        }
+        scroll();
+
+        window.addEventListener('scroll', scroll);
+        return () => window.removeEventListener('scroll', scroll);
+    }, []);
+
+    const className = [
+        styles['tile'],
+        isHidden ? styles['hidden'] : ''
+    ].join(' ');
+    return(
+        <div className={className}>
+            <div className={styles['tile-content']} ref={ref}>
+                <div className={styles['tile-text']}>
+                    <h1>
+                        {title}
+                    </h1>
+                    <p>
+                        {description}
+                    </p>
+                </div>
+                <div className={styles['tile-media']}>
+                    {media}
+                </div>
+            </div>
+        </div>
+    )
+}
