17498624b48673fe3571543ad31df39226e55599
Poxie <poxen12@hotmail.com>
2022-05-26 16:27:04 +0200

    Added new icons

---
+++ frontend/icons/SequenceIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const SequenceIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99" fill="none">
+            <rect width="45.7189" height="45.7189" rx="8" fill="#5D20C2"/>
+            <rect x="52.7189" width="45.7189" height="45.7189" rx="8" fill="#5D20C2"/>
+            <rect x="52.7189" y="52.719" width="45.7189" height="45.7189" rx="8" fill="#5D20C2"/>
+            <rect x="60" y="60" width="32" height="32" rx="8" fill="white"/>
+            <rect y="52.719" width="45.7189" height="45.7189" rx="8" fill="#5D20C2"/>
+        </svg>
+    )
+}
