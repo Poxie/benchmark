90aa8730ec918fcec43396f5d20e44c63526f77a
Poxie <poxen12@hotmail.com>
2022-05-27 04:34:08 +0200

    Added extra properties to button component

--- frontend/components/button/index.tsx
+++ frontend/components/button/index.tsx
@@ -4,10 +4,17 @@ import styles from '../../styles/Button.module.scss';
 export const Button: React.FC<{
     children: any;
     onClick?: () => void;
-}> = ({ onClick, children }) => {
+    type?: 'primary' | 'light';
+    className?: string;
+}> = ({ onClick, children, type='primary', className }) => {
+    className = [
+        styles['container'],
+        styles[type],
+        className
+    ].join(' ');
     return(
         <div 
-            className={styles['container']}
+            className={className}
             onClick={onClick}
         >
             {children}
