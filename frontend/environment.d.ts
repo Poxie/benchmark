646aeedf8c441d1dad4ea0384cf64a5a8ed7a8d3
Poxie <poxen12@hotmail.com>
2022-05-27 02:53:05 +0200

    Added types for new .env variables

--- frontend/environment.d.ts
+++ frontend/environment.d.ts
@@ -3,6 +3,8 @@ declare global {
         interface ProcessEnv {
             NODE_ENV: 'development' | 'production';
             NEXT_PUBLIC_WEBSITE_NAME: string;
+            NEXT_PUBLIC_WEBSITE_DESCRIPTION: string;
+            NEXT_PUBLIC_CONTACT_EMAIL: string;
         }
     }
 }
