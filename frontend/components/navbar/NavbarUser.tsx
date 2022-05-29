8e49c2f6d3fcf2f89eeca5917052fd511fee3768
Poxie <poxen12@hotmail.com>
2022-05-29 15:09:35 +0200

    Centering navbar user options on mobile devices

--- frontend/components/navbar/NavbarUser.tsx
+++ frontend/components/navbar/NavbarUser.tsx
@@ -3,17 +3,19 @@ import styles from '../../styles/Navbar.module.scss';
 import { usePopup } from '../../contexts/PopupProvider';
 import { NavbarUserPopup } from '../../popups/navbar-user/NavbarUserPopup';
 import { User } from '../../redux/user/types';
+import { useDeviceType } from '../../hooks/useDeviceType';
 
 export const NavbarUser: React.FC<{
     user: User;
 }> = ({ user: { id, username } }) => {
     const { setPopup } = usePopup();
     const ref = useRef<HTMLDivElement>(null);
+    const deviceType = useDeviceType();
 
     return(
         <div 
             className={styles['user']}
-            onClick={() => setPopup(<NavbarUserPopup />, ref)} 
+            onClick={() => setPopup(<NavbarUserPopup />, ref, { centered: deviceType === 'mobile' })} 
             ref={ref}
         >
             {username}
