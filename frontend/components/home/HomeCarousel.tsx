f35c66c1ff2cb0bdd1573f5ffa78fa7cf72ecd5e
Poxie <poxen12@hotmail.com>
2022-05-31 18:50:47 +0200

    Updated home carousel items

--- frontend/components/home/HomeCarousel.tsx
+++ frontend/components/home/HomeCarousel.tsx
@@ -11,10 +11,10 @@ import { Carousel } from '../carousel/Carousel';
 import { HomeCarouselItem } from './HomeCarouselItem';
 
 const items = [
-    { text: 'Memory Sequence', category: 'Memory', path: '/memory-sequence', icon: SequenceIcon, comingSoon: true },
+    { text: 'Memory Sequence', category: 'Memory', path: '/memory-sequence', icon: SequenceIcon, comingSoon: false, isBeta: true },
     { text: 'Chimp Test', category: 'Memory', path: '/chimp-test', icon: ChimpIcon, comingSoon: true },
     { text: 'Number Memory', category: 'Memory', path: '/number-memory', icon: NumberMemoryIcon, comingSoon: false, isBeta: true },
-    { text: 'Word Memory', category: 'Memory', path: '/word-memory', icon: WordMemoryIcon, comingSoon: true },
+    { text: 'Word Memory', category: 'Memory', path: '/word-memory', icon: WordMemoryIcon, comingSoon: false, isBeta: true },
     { text: 'Aim Training', category: 'Aim', path: '/aim-training', icon: AimTrainingIcon, comingSoon: true },
     { text: 'Typing Speed', category: 'Typing', path: '/typing-speed', icon: TypingIcon, comingSoon: true },
     { text: 'Reaction Time', category: 'Reaction', path: '/reaction-time', icon: ReactionTimeIcon, comingSoon: true }
