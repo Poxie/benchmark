89353e3573f175d57b2aa544895befb6ca89b3d7
Poxie <poxen12@hotmail.com>
2022-06-10 05:23:54 +0200

    Made sidebar and overview page responsive

--- frontend/components/profile/sidebar/ProfileSidebar.tsx
+++ frontend/components/profile/sidebar/ProfileSidebar.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useDeviceType } from '../../../hooks/useDeviceType';
 import styles from '../../../styles/Profile.module.scss';
 import { ProfileSidebarTabs } from './ProfileSidebarSections';
 import { ProfileSidebarTop } from './ProfileSidebarTop';
 
 export const ProfileSidebar = () => {
+    const [active, setActive] = useState(false);
+    const gameId = useRouter().query.gameId;
+    const device = useDeviceType();
+
+    // Toggling active state on smaller devices
+    const toggleActive = () => setActive(!active);
+
+    // Closing if user press tab
+    useEffect(toggleActive, [gameId]);
+
+    // Closing if user screen extends
+    useEffect(() => {
+        if(device === 'computer') setActive(false);
+    }, [device]);
+
+    // Determining sidebar className
+    const className = [
+        styles['sidebar'],
+        active ? styles['active'] : ''
+    ].join(' ');
     return(
-        <div className={styles['sidebar']}>
+        <div className={className}>
             <div className={styles['sidebar-content']}>
-                <ProfileSidebarTop />
+                <ProfileSidebarTop 
+                    onClick={device !== 'computer' ? toggleActive : undefined}
+                />
                 <ProfileSidebarTabs />
             </div>
         </div>
--- frontend/components/profile/sidebar/ProfileSidebarTop.tsx
+++ frontend/components/profile/sidebar/ProfileSidebarTop.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import styles from '../../../styles/Profile.module.scss';
 import { selectProfileIdentity, selectProfileIsLoading } from '../../../redux/profile/selectors';
 import { useAppSelector } from '../../../redux/store';
+import { useDeviceType } from '../../../hooks/useDeviceType';
+import { FilledArrowIcon } from '../../../icons/FilledArrowIcon';
 
-export const ProfileSidebarTop = () => {
+export const ProfileSidebarTop: React.FC<{
+    onClick: (() => void) | undefined;
+}> = ({ onClick }) => {
+    const device = useDeviceType();
     const loading = useAppSelector(selectProfileIsLoading);
     const identity = useAppSelector(selectProfileIdentity);
 
     if(loading || !identity) return null;
 
     return(
-        <div className={styles['sidebar-top']}>
+        <div 
+            className={styles['sidebar-top']} 
+            onClick={onClick}
+        >
             {/* Once profile avatar is available, add conditional logic for it here */}
             {/* <div className={styles['avatar']}>
                 ?
@@ -18,6 +26,9 @@ export const ProfileSidebarTop = () => {
             <span className={styles['username']}>
                 {identity.username}
             </span>
+            {device !== 'computer' && (
+                <FilledArrowIcon />
+            )}
         </div>
     )
 }
