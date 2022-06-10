163cee51dec132ba054318031fca3c1c33521e19
Poxie <poxen12@hotmail.com>
2022-06-10 06:12:25 +0200

    Added popup item

--- frontend/popups/navbar-user/NavbarUserPopup.tsx
+++ frontend/popups/navbar-user/NavbarUserPopup.tsx
@@ -1,13 +1,22 @@
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useAppSelector } from '../../redux/store';
+import { selectUserInfo } from '../../redux/user/selectors';
 import { NavbarUserPopupItem } from './NavbarUserPopupItem';
 
 export const NavbarUserPopup = () => {
+    const router = useRouter();
+    const username = useAppSelector(selectUserInfo)?.username;
+
+    const redirect = (path: string) => router.push(path);
+    
     const logout = () => {
         window.localStorage.removeItem('token');
         window.location.reload();
     }
 
     const items = [
+        { text: 'Profile', onClick: () => redirect(`/profile/${username}/overview`) },
         { text: 'Log out', type: 'danger', onClick: logout }
     ]
     return(
