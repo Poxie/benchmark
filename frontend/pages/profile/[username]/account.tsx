e117a202d4c7de132f6f6b94274cdad362f2ade7
Poxie <poxen12@hotmail.com>
2022-06-10 06:38:02 +0200

    Added basic account page

---
+++ frontend/pages/profile/[username]/account.tsx
@@ -0,0 +1,64 @@
+import { useRouter } from 'next/router';
+import React, { ReactElement } from 'react';
+import { connect } from 'react-redux';
+import { Account } from '../../../components/profile/account/Account';
+import { GET_PROFILE_BY_USERNAME } from '../../../graphql/queries';
+import { ProfileLayout } from '../../../layouts/ProfileLayout';
+import { setProfile } from '../../../redux/profile/actions';
+import { selectProfileIsLoading, selectProfileIsMe } from '../../../redux/profile/selectors';
+import { RootState, useAppSelector, wrapper } from '../../../redux/store';
+import { selectUserIsLoading } from '../../../redux/user/selectors';
+import { client } from '../../_app';
+
+export const getServerSideProps = wrapper.getServerSideProps(store => async ({ query }) => {
+    const { username } = query;
+
+    // Fetching user overview
+    const { data } = await client.query({
+        query: GET_PROFILE_BY_USERNAME,
+        variables: {
+            username
+        }
+    })
+    const profile = data?.getProfileOverview;
+
+    // If user not found
+    if(!profile) return {
+        notFound: true
+    }
+    
+    // Updating redux store
+    store.dispatch(setProfile(profile));
+
+    return {
+        props: {
+            profile
+        }
+    }
+});
+const account = () => {
+    const meIsLoading = useAppSelector(selectUserIsLoading);
+    const profileIsLoading = useAppSelector(selectProfileIsLoading);
+    const isMe = useAppSelector(selectProfileIsMe);
+
+    // Checking whether user is authorized to view
+    if(meIsLoading || profileIsLoading) return null;
+    if(!isMe) {
+        return(
+            <div>
+                Unauthorized.
+            </div>
+        )
+    }
+
+    return <Account />;
+}
+account.getLayout = (page: ReactElement) => {
+    return(
+        <ProfileLayout>
+            {page}
+        </ProfileLayout>
+    )
+}
+
+export default connect((state: RootState) => state)(account);
