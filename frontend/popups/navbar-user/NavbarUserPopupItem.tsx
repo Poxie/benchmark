87db375b23ca34bad630e76fd2db18762e3233b6
Poxie <poxen12@hotmail.com>
2022-06-10 06:12:48 +0200

    Fixed typing, closing popup on click

--- frontend/popups/navbar-user/NavbarUserPopupItem.tsx
+++ frontend/popups/navbar-user/NavbarUserPopupItem.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { usePopup } from '../../contexts/PopupProvider';
 import styles from './NavbarUserPopup.module.scss';
 
 export const NavbarUserPopupItem: React.FC<{
     text: string;
     onClick: () => void;
-    type: string;
-}> = ({ text, onClick, type }) => {
+    type?: string;
+}> = ({ text, onClick, type='default' }) => {
+    const { closePopups } = usePopup();
+
     const className = [
         styles['item'],
         styles[type]
     ].join(' ');
     return(
-        <div className={className} onClick={onClick}>
+        <div className={className} onClick={() => {
+            onClick();
+            closePopups();
+        }}>
             {text}
         </div>
     )
