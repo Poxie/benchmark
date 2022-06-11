0b34bf95f1d942c0df7e465e39ddcabf58a060a9
Poxie <poxen12@hotmail.com>
2022-06-11 08:11:20 +0200

    Updated chimp test carousel status

--- frontend/components/home/HomeCarousel.tsx
+++ frontend/components/home/HomeCarousel.tsx
@@ -12,7 +12,7 @@ import { HomeCarouselItem } from './HomeCarouselItem';
 
 const items = [
     { text: 'Memory Sequence', category: 'Memory', path: '/memory-sequence', icon: SequenceIcon, comingSoon: false, isBeta: true },
-    { text: 'Chimp Test', category: 'Memory', path: '/chimp-test', icon: ChimpIcon, comingSoon: true },
+    { text: 'Chimp Test', category: 'Memory', path: '/chimp-test', icon: ChimpIcon, comingSoon: false, isBeta: true },
     { text: 'Number Memory', category: 'Memory', path: '/number-memory', icon: NumberMemoryIcon, comingSoon: false, isBeta: true },
     { text: 'Word Memory', category: 'Memory', path: '/word-memory', icon: WordMemoryIcon, comingSoon: false, isBeta: true },
     { text: 'Aim Training', category: 'Aim', path: '/aim-training', icon: AimTrainingIcon, comingSoon: true },
