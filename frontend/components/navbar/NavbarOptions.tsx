7933396daafb238a6f13dea6fdbc2b49bcffa09e
Poxie <poxen12@hotmail.com>
2022-06-13 20:40:14 +0200

    Showing settings to non logged in users

--- frontend/components/navbar/NavbarOptions.tsx
+++ frontend/components/navbar/NavbarOptions.tsx
@@ -1,12 +1,18 @@
+import React, { useRef } from 'react';
+import styles from '../../styles/Navbar.module.scss';
 import { useRouter } from 'next/router';
-import React from 'react';
+import { GearIcon } from '../../icons/GearIcon';
 import { useAppSelector } from '../../redux/store';
 import { selectUserInfo, selectUserIsLoading } from '../../redux/user/selectors';
 import { Button } from '../button';
 import { NavbarUser } from './NavbarUser';
+import { usePopup } from '../../contexts/PopupProvider';
+import { SettingsPopup } from '../../popups/settings/SettingsPopup';
 
 export const NavbarOptions = () => {
     const router = useRouter();
+    const { setPopup } = usePopup();
+    const settingsRef = useRef<HTMLDivElement>(null);
     const loading = useAppSelector(selectUserIsLoading);
     const userInfo = useAppSelector(selectUserInfo);
 
@@ -17,9 +23,18 @@ export const NavbarOptions = () => {
         userInfo ? (
             <NavbarUser user={userInfo} />
         ) : (
-            <Button onClick={() => router.push(`/login?redirect_uri=${encodeURIComponent(router.asPath)}`)}>
-                Sign in
-            </Button>
+            <div className={styles['right']}>
+                <div 
+                    className={styles['settings-button']} 
+                    onClick={() => setPopup(<SettingsPopup />, settingsRef)}
+                    ref={settingsRef}
+                >
+                    <GearIcon />
+                </div>
+                <Button onClick={() => router.push(`/login?redirect_uri=${encodeURIComponent(router.asPath)}`)}>
+                    Sign in
+                </Button>
+            </div>
         )
     )
 }
