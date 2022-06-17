1eaea524cc405d6fe6ff9c2fbaca9c057a7f6508
Poxie <poxen12@hotmail.com>
2022-06-17 16:27:10 +0200

    Added label on leaderboard for 'You' if you are logged in

--- frontend/components/game-components/LeaderboardItem.tsx
+++ frontend/components/game-components/LeaderboardItem.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
 import React from 'react';
+import { useAppSelector } from '../../redux/store';
+import { selectUserInfo } from '../../redux/user/selectors';
 import styles from '../../styles/GameComponents.module.scss';
 import { Score } from './Leaderboard';
 
 export const LeaderboardItem: React.FC<Score> = ({ ranking, score, user }) => {
+    const loggedInUser = useAppSelector(selectUserInfo);
+    const isMe = loggedInUser?.id === user.id;
+
     return(
         <div className={styles['leaderboard-item']}>
             <div className={styles['leaderboard-item-main']}>
@@ -17,6 +22,11 @@ export const LeaderboardItem: React.FC<Score> = ({ ranking, score, user }) => {
                     <Link href={`/profile/${user.username}/overview`}>
                         {user.name || user.username}
                     </Link>
+                    {isMe && (
+                        <span className={styles['is-me']}>
+                            You
+                        </span>
+                    )}
                 </span>
             </div>
             <span className={styles['leaderboard-score']}>
